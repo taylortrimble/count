1434ce92b359fc402ccc0c1426385b2dbfb03947
Taylor Trimble <tylrtrmbl@gmail.com>
2011-09-27 22:18:40 -0500

    Initial Commit

---
+++ Count/TextDocument.h
@@ -0,0 +1,14 @@
+//
+//  TextDocument.h
+//  Count
+//
+//  Created by Taylor Trimble on 9/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TextDocument : NSDocument {
+}
+
+@end
---
+++ Count/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Count
+//
+//  Created by Taylor Trimble on 9/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
